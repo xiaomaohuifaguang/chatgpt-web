8f0bffabc0f950da1142e6dd61ce1a750e906a32
小猫会发光 <121420990+xiaomaohuifaguang@users.noreply.github.com>
2023-08-01 01:00:00 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: '小猫会发光',
+      description: 'Star on <a href="https://github.com/xiaomaohuifaguang/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
